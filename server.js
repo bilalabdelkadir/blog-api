b0e20a9cc1226483739a4d05df05737fb364641b
bilalabdelkadir <bilalbinabdelkadir>
2023-05-10 09:30:24 +0300

    feat: configured based server

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDatabase from "./config/databaseConfig.js";
+
+// database connection
+connectDatabase();
+dotenv.config({ path: "./config/config.env" });
+
+// setting up the middleware
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
+
+const server = app.listen(process.env.PORT, () =>
+  console.log(
+    `server started at port ${process.env.PORT} in ${process.env.NODE_ENV}`
+  )
+);
