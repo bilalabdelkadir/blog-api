81c548041d601f63588da133465908cbd0f86f59
bilalabdelkadir <bilalbinabdelkadir>
2023-05-10 22:49:41 +0300

    feat: added script to run docker and close

---
+++ run_docker.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Start Docker Compose
+docker compose up
+
+# Log Message
+echo "- - - - - - - - - - - - - - - - - - -"
+echo "Docker Compose file run successfully"
+echo "- - - - - - - - - - - - - - - - - - -"
---
+++ stop_docker.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Stop Docker Compose
+docker compose down
+
+# Log Message
+echo "- - - - - - - - - - - - - - - - - - - - -"
+echo "Docker Compose file stopped successfully"
+echo "- - - - - - - - - - - - - - - - - - - - -"
